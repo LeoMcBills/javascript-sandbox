dba8494d982d69398cf4fa9be2eaf02e8f13a980
LeoMcBills <leokinyera9@gmail.com>
2024-02-01 11:12:30 +0300

    auto pushed

--- practice/script.js
+++ practice/script.js
@@ -3,9 +3,22 @@
 // console.log(parent)
 console.log("DOM Practice")
 
-const parent = document.getElementById("heading")
+// const parent = document.getElementById("heading")
 
-console.log(parent)
+// console.log(parent)
 
-parent.alert
+// parent.alert
+
+const elem = document.querySelector(".btn")
+
+console.log(elem)
+
+const title = document.getElementById('heading');
+
+// Get/change content
+console.log(title.textContent);
+title.textContent = 'Hello World';
+title.innerText = 'Hello Again';
+// title.innerHTML = '<strong>Shopping List</strong>';
+
 
